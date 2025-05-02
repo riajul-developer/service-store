8b74fa898a7892129de15fbdac480efe7b628ef9
Riajul Islam <raihan.dev3@gmail.com>
2025-05-02 15:37:32 +0600

    some util file added

--- utils/response_util.go
+++ utils/response_util.go
@@ -5,7 +5,7 @@ import (
 )
 
 type APIResponse struct {
-	Status  string      `json:"status"` // "success" or "error"
+	Status  string      `json:"status"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
 	Errors  interface{} `json:"errors,omitempty"`
@@ -45,26 +45,26 @@ func ErrorResponse(c *fiber.Ctx, statusCode int, message string, errs interface{
 }
 
 // Error Shortcut Wrappers
-func BadRequest(c *fiber.Ctx, message string, errs interface{}) error {
-	return ErrorResponse(c, fiber.StatusBadRequest, message, errs)
-}
+// func BadRequest(c *fiber.Ctx, message string, errs interface{}) error {
+// 	return ErrorResponse(c, fiber.StatusBadRequest, message, errs)
+// }
 
-func NotFound(c *fiber.Ctx, message string) error {
-	return ErrorResponse(c, fiber.StatusNotFound, message, nil)
-}
+// func NotFound(c *fiber.Ctx, message string) error {
+// 	return ErrorResponse(c, fiber.StatusNotFound, message, nil)
+// }
 
-func ServerError(c *fiber.Ctx, message string) error {
-	return ErrorResponse(c, fiber.StatusInternalServerError, message, nil)
-}
+// func ServerError(c *fiber.Ctx, message string) error {
+// 	return ErrorResponse(c, fiber.StatusInternalServerError, message, nil)
+// }
 
-func Unauthorized(c *fiber.Ctx, message string) error {
-	return ErrorResponse(c, fiber.StatusUnauthorized, message, nil)
-}
+// func Unauthorized(c *fiber.Ctx, message string) error {
+// 	return ErrorResponse(c, fiber.StatusUnauthorized, message, nil)
+// }
 
-func Forbidden(c *fiber.Ctx, message string) error {
-	return ErrorResponse(c, fiber.StatusForbidden, message, nil)
-}
+// func Forbidden(c *fiber.Ctx, message string) error {
+// 	return ErrorResponse(c, fiber.StatusForbidden, message, nil)
+// }
 
-func Conflict(c *fiber.Ctx, message string, errs interface{}) error {
-	return ErrorResponse(c, fiber.StatusConflict, message, errs)
-}
+// func Conflict(c *fiber.Ctx, message string, errs interface{}) error {
+// 	return ErrorResponse(c, fiber.StatusConflict, message, errs)
+// }
